0f70d231cc783b0e749571a6bdd627530e28fdb6
Bohdan Yevtushenko <evtushencko.b@gmail.com>
2020-09-11 23:07:53 +0300

    New version.

---
+++ android/app/src/main/java/com/example/vkdonations/ChooseTypeDonationActivity.kt
@@ -0,0 +1,22 @@
+package com.example.vkdonations
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_choose_type_donation.*
+import kotlinx.android.synthetic.main.toolbar_layout.*
+import kotlinx.android.synthetic.main.toolbar_layout.tvTitle
+
+class ChooseTypeDonationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_type_donation)
+
+        tvTitle.text = getString(R.string.type_of_donation)
+
+        cvDonationWithGoal.setOnClickListener {
+            startActivity(Intent(this, CreateDonationWithGoalActivity::class.java))
+        }
+        cvRegularDonation.setOnClickListener { println("regular") }
+    }
+}
